4a6fdbad85fac34fb27ff2bdd9f3b4797347ff42
Den <danielranieri06@gmail.com>
2026-05-30 21:08:50 +0200

    Added db_config module
    Added .env file

---
+++ persistence/db_config.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from dotenv import load_dotenv
+import os
+
+class Base(DeclarativeBase):
+    pass
+
+USERNAME = os.getenv("DB_USERNAME")
+PASSWORD = os.getenv("DB_PASSWORD")
+URL = os.getenv("DB_URL")
+DATABASE_NAME = os.getenv("DB_NAME")
+
+engine = create_engine(f"postgresql://{USERNAME}:{PASSWORD}@{URL}/{DATABASE_NAME}")
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+def init_db():
+
+    Base.metadata.create_all(bind=engine)
+
+
+def get_session():
+    return SessionLocal()
